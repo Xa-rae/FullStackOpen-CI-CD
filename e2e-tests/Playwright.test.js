9e16903bb5306b7386b55dd129e005e037760ebc
Xa-rae <Nikita.tcherednitchenko@gmail.com>
2026-09-16 16:29:25 +0300

    deployment

--- e2e-tests/Playwright.test.js
+++ e2e-tests/Playwright.test.js
@@ -6,4 +6,4 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-})/*  */
+})
